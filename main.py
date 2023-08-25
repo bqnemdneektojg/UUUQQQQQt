4fbea6fff667ee85b9204075d5e7119c084061a9
bqnemdneektojg <141013246+bqnemdneektojg@users.noreply.github.com>
2023-08-25 21:59:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# ------------ Модули ----------#
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart 
+import MIMEMultipart
+import telebot
+# ------------ Связь с Telegram API -------- #
+TOKEN = config.TOKEN_BOT
+bot = telebot.TeleBot(TOKEN)
+admin = config.admin
+# ----------- 
